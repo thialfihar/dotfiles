3e17b7b91291bd1e51eac1b5aa304fac5faf4d60
Thialfihar <thi@thialfihar.org>
2014-06-27 18:17:52 +0200

    Add experimental i3 layout restoring

---
+++ i3/i3/i3-load-layout-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+#i3-msg 'focus parent; focus parent; focus parent; focus parent; kill'
+i3-msg 'append_layout /home/thi/.i3/layout.dev.json'
+
+#gnome-terminal &
+#gnome-terminal &
+#st &
